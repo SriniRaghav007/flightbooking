970006e4ac3d8e285a22165b0eb2e8866f25f446
Vihar Devalla <vihardevalla@gmail.com>
2020-11-17 21:22:04 +0530

    .
    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
+import { AiFillLinkedin } from 'react-icons/ai';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="back">
+        <div className="back2">
+          <div className="searchbox">
+          <h3>Book Flights</h3>
+          <form className="form-group">
+          <label >
+                From : 
+                <input type="text" name="from" />
+              </label><br/>
+              <label>
+                To : 
+                <input type="text" name="from" />
+              </label><br/>
+              <label>Departure Date :
+              <input type="date" name="date1"  /><br></br>
+              </label>
+              <label>Return Date :
+              <input type="date" name="date2"  /><br></br>
+              </label>
+              <input type="submit" value="Submit" />
+          </form>
+
+          </div>
+        </div>
+      </div>
   );
 }
+
+/* 
+<form>
+              <label>
+                From:
+                <input type="text" name="from" />
+              </label><br/>
+              <label>
+                To:
+                <input type="text" name="from" />
+              </label><br/>
+              <label>Departure Date:
+              <input type="date" name="date1"  /><br></br>
+              </label>
+              <label>Return Date
+              <input type="date" name="date2"  /><br></br>
+              </label>
+              <input type="submit" value="Submit" />
+            </form> 
+*/
 
 export default App;
